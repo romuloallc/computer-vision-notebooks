89040e9aaa5a3382173d10efcce11068d5a82d71
Diego Inácio <diegodci@gmail.com>
2021-03-04 18:59:52 -0300

    Update page scripts index Closes #9

--- docs/scripts/index.js
+++ docs/scripts/index.js
@@ -18,38 +18,101 @@ enquirer
       type: "select",
       name: "option",
       message: "Select option: ",
-      choices: ["Metatag", "Style", "Navbar"],
-    },
-    {
-      type: "select",
-      name: "action",
-      message: "Select action: ",
-      choices: ["Show", "Include", "Remove"],
-    },
-    {
-      type: "multiselect",
-      name: "notebooks",
-      message: "Select notebooks: ",
-      limit: 5,
-      choices: NOTEBOOKS.slice(0),
+      choices: ["Do all the processes?", "Do a specific process?"],
     },
   ])
   .then((answers) => {
     switch (answers.option) {
-      case "Metatag":
-        // ! Metatag option selected
-        metatag.metatag_option(answers, NOTEBOOKS);
+      case "Do all the processes?":
+        all_processes();
         break;
-      case "Style":
-        // ! Style option selected
-        style.style_option(answers, NOTEBOOKS);
-        break;
-      case "Navbar":
-        // ! Style option selected
-        navbar.navbar_option(answers, NOTEBOOKS);
+      case "Do a specific process?":
+        specific_process();
         break;
       default:
         // ! No option selected
         console.log(chalk.bold.red("No option was selected!"));
     }
   });
+
+function all_processes() {
+  // ! Execute all the processes
+  enquirer
+    .prompt([
+      {
+        type: "multiselect",
+        name: "notebooks",
+        message: "Select notebooks: ",
+        limit: 5,
+        choices: NOTEBOOKS.slice(0),
+      },
+    ])
+    .then((answers) => {
+      // * Removes
+      answers.action = "Remove";
+      answers.option = "Metatag";
+      console.log(chalk.bold.yellow("Removing metatags .."));
+      metatag.metatag_option(answers, NOTEBOOKS);
+      answers.option = "Style";
+      console.log(chalk.bold.yellow("Removing styles .."));
+      style.style_option(answers, NOTEBOOKS);
+
+      // * Includes
+      answers.action = "Include";
+      answers.option = "Navbar";
+      console.log(chalk.bold.yellow("Including navbar .."));
+      navbar.navbar_option(answers, NOTEBOOKS);
+      answers.option = "Style";
+      console.log(chalk.bold.yellow("Including styles .."));
+      style.style_option(answers, NOTEBOOKS);
+      answers.option = "Metatag";
+      console.log(chalk.bold.yellow("Including metatags .."));
+      metatag.metatag_option(answers, NOTEBOOKS);
+    });
+}
+
+function specific_process() {
+  // ! Execute a specific process
+  enquirer
+    .prompt([
+      {
+        type: "select",
+        name: "option",
+        message: "Select option: ",
+        choices: ["Metatag", "Style", "Navbar"],
+      },
+      {
+        type: "select",
+        name: "action",
+        message: "Select action: ",
+        choices: ["Show", "Include", "Remove"],
+      },
+      {
+        type: "multiselect",
+        name: "notebooks",
+        message: "Select notebooks: ",
+        limit: 5,
+        choices: NOTEBOOKS.slice(0),
+      },
+    ])
+    .then((answers) => {
+      console.log(answers);
+      switch (answers.option) {
+        case "Metatag":
+          // ! Metatag option selected
+          metatag.metatag_option(answers, NOTEBOOKS);
+          break;
+        case "Style":
+          // ! Style option selected
+          style.style_option(answers, NOTEBOOKS);
+          break;
+        case "Navbar":
+          // ! Style option selected
+          navbar.navbar_option(answers, NOTEBOOKS);
+          break;
+        default:
+          // ! No option selected
+          console.log(chalk.bold.red("No option was selected!"));
+      }
+    });
+}
